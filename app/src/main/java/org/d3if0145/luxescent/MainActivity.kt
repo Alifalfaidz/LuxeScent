975e2635607b09d0cf5b8e0ed0ed1545e8019fea
Alif Mahtum Alfaidz <alif.alfaidz@gmail.com>
2024-03-29 15:58:05 +0700

    Top bar sudah di pisah

--- app/src/main/java/org/d3if0145/luxescent/MainActivity.kt
+++ app/src/main/java/org/d3if0145/luxescent/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import org.d3if0145.luxescent.ui.screen.MainScreen
 import org.d3if0145.luxescent.ui.theme.LuxeScentTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,50 +30,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit){
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    )
-    {
-            padding -> content(Modifier.padding(padding))
-    }
-}
-
-
-@Composable
-fun Greeting(name: String) {
-    MainScreen { modifier ->
-        Text(text = "Hello $name!",
-        modifier = modifier
-        )
-    }
-}
-
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LuxeScentTheme {
-        Greeting("Android")
-    }
-}
